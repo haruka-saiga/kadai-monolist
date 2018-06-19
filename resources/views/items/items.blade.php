7b601e8221b98ea3b07020a5b631efdd7080a79b
haruka.saiga <ec2-user@ip-172-31-26-50.us-east-2.compute.internal>
2018-06-19 04:27:28 +0000

    count

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,6 +1,6 @@
 @if ($items)
     <div class="row">
-        @foreach ($items as $item)
+        @foreach ($items as $key => $item)
             <div class="item">
                 <div class="col-md-3 col-sm-4 col-xs-12">
                     <div class="panel panel-default">
@@ -28,6 +28,18 @@
                             </span>
                             </td>
                         </div>
+                        
+                        @if (isset($item->want_count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->want_count}} Wants</p>
+                            </div>
+                        @endif
+                        
+                        @if (isset($item->have_count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->have_count}} Has</p>
+                            </div>
+                        @endif
                     </div>
                 </div>
             </div>
